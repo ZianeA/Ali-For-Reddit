f9d9a63a4fa60efff788da360eef0c48672e27d8
ZianeA <wit_wit90@hotamail.com>
2019-10-15 12:38:23 +0100

    Fix RoundedImageView corner radius not working

--- app/src/main/java/com/visualeap/aliforreddit/presentation/util/RoundedImageView.kt
+++ app/src/main/java/com/visualeap/aliforreddit/presentation/util/RoundedImageView.kt
@@ -7,6 +7,7 @@ import androidx.core.graphics.drawable.RoundedBitmapDrawableFactory
 import androidx.core.graphics.drawable.toBitmap
 import android.graphics.Canvas
 import android.graphics.drawable.Drawable
+import android.widget.ImageView
 import com.visualeap.aliforreddit.R
 
 
@@ -53,22 +54,37 @@ class RoundedImageView : AppCompatImageView {
         }
 
         //Request another setImageDrawable because isCircular and cornerRadius were not set yet when this method was initially called
-        setImageDrawable(drawable)
+//        setImageDrawable(drawable)
     }
 
     override fun setImageDrawable(drawable: Drawable?) {
         if (drawable != null && drawable.intrinsicWidth > 0) {
+            // When the bitmap's height and width are not the same as the ImageView's height and width, corner radius won't work as expected.
+            // Bitmap and ImageView should have the same size only when the scale type is set to FIT_CENTER
+            // We have to wait for size to be calculated. If we attempt to Create a bitmap with a size of 0, an exception will be thrown
+            val bitmap =
+                if (width > 0 && height > 0 && scaleType == ScaleType.FIT_CENTER)
+                    drawable.toBitmap(width, height)
+                else drawable.toBitmap()
+
             val roundedDrawable =
-                RoundedBitmapDrawableFactory.create(
-                    resources,
-                    drawable.toBitmap()
-                )
+                RoundedBitmapDrawableFactory.create(resources, bitmap)
             roundedDrawable.cornerRadius = cornerRadius.toFloat()
-            roundedDrawable.isCircular = isCircular
+
+            if (isCircular) {
+                roundedDrawable.isCircular = isCircular
+            }
+
             super.setImageDrawable(roundedDrawable)
         } else {
             super.setImageDrawable(drawable)
         }
+    }
+
+    override fun onSizeChanged(w: Int, h: Int, oldw: Int, oldh: Int) {
+        super.onSizeChanged(w, h, oldw, oldh)
+        //When the size changes we need to request another setImageDrawable in order for corner radius to work.
+        setImageDrawable(drawable)
     }
 
     companion object {
