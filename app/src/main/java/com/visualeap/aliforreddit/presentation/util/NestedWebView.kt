585892d9693abda89e4e208d26bb8985ebff7017
ZianeA <wit_wit90@hotamail.com>
2019-12-07 20:05:54 +0100

    Fix bottom navigation bar obscuring login webView

---
+++ app/src/main/java/com/visualeap/aliforreddit/presentation/util/NestedWebView.kt
@@ -0,0 +1,162 @@
+/*
+ * Copyright (C) 2015 takahirom
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *      http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.visualeap.aliforreddit.presentation.util
+
+import android.content.Context
+import android.util.AttributeSet
+import android.view.MotionEvent
+import android.webkit.WebView
+import androidx.core.view.MotionEventCompat
+import androidx.core.view.NestedScrollingChild
+import androidx.core.view.NestedScrollingChildHelper
+import androidx.core.view.ViewCompat
+import com.visualeap.aliforreddit.R
+
+class NestedWebView : WebView, NestedScrollingChild {
+    private var mLastY = 0
+    private val mScrollOffset = IntArray(2)
+    private val mScrollConsumed = IntArray(2)
+    private var mNestedOffsetY = 0
+    private val mChildHelper: NestedScrollingChildHelper = NestedScrollingChildHelper(this)
+
+    constructor(context: Context) : super(context) {
+        init(context, null, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?) : super(context, attrs) {
+        init(context, attrs, 0)
+    }
+
+    constructor(context: Context, attrs: AttributeSet?, defStyleAttr: Int) : super(
+        context,
+        attrs,
+        defStyleAttr
+    ) {
+        init(context, attrs, defStyleAttr)
+    }
+
+    private fun init(
+        context: Context,
+        attrs: AttributeSet?,
+        defStyleAttr: Int,
+        defStyleRes: Int = 0
+    ) {
+        isNestedScrollingEnabled = true;
+    }
+
+    override fun onTouchEvent(ev: MotionEvent): Boolean {
+        var returnValue = false
+        val event = MotionEvent.obtain(ev)
+        val action = MotionEventCompat.getActionMasked(event)
+        if (action == MotionEvent.ACTION_DOWN) {
+            mNestedOffsetY = 0
+        }
+        val eventY = event.y.toInt()
+        event.offsetLocation(0f, mNestedOffsetY.toFloat())
+        when (action) {
+            MotionEvent.ACTION_MOVE -> {
+                var deltaY = mLastY - eventY
+                // NestedPreScroll
+                if (dispatchNestedPreScroll(0, deltaY, mScrollConsumed, mScrollOffset)) {
+                    deltaY -= mScrollConsumed[1]
+                    mLastY = eventY - mScrollOffset[1]
+                    event.offsetLocation(0f, (-mScrollOffset[1]).toFloat())
+                    mNestedOffsetY += mScrollOffset[1]
+                }
+                returnValue = super.onTouchEvent(event)
+                // NestedScroll
+                if (dispatchNestedScroll(0, mScrollOffset[1], 0, deltaY, mScrollOffset)) {
+                    event.offsetLocation(0f, mScrollOffset[1].toFloat())
+                    mNestedOffsetY += mScrollOffset[1]
+                    mLastY -= mScrollOffset[1]
+                }
+            }
+            MotionEvent.ACTION_DOWN -> {
+                returnValue = super.onTouchEvent(event)
+                mLastY = eventY
+                // start NestedScroll
+                startNestedScroll(ViewCompat.SCROLL_AXIS_VERTICAL)
+            }
+            MotionEvent.ACTION_UP, MotionEvent.ACTION_CANCEL -> {
+                returnValue = super.onTouchEvent(event)
+                // end NestedScroll
+                stopNestedScroll()
+            }
+        }
+        return returnValue
+    }
+
+    // Nested Scroll implements
+    override fun setNestedScrollingEnabled(enabled: Boolean) {
+        mChildHelper.isNestedScrollingEnabled = enabled
+    }
+
+    override fun dispatchNestedScroll(
+        dxConsumed: Int,
+        dyConsumed: Int,
+        dxUnconsumed: Int,
+        dyUnconsumed: Int,
+        offsetInWindow: IntArray?
+    ): Boolean {
+        return mChildHelper.dispatchNestedScroll(
+            dxConsumed,
+            dyConsumed,
+            dxUnconsumed,
+            dyUnconsumed,
+            offsetInWindow
+        )
+    }
+
+    override fun isNestedScrollingEnabled(): Boolean {
+        return mChildHelper.isNestedScrollingEnabled
+    }
+
+    override fun dispatchNestedPreScroll(
+        dx: Int,
+        dy: Int,
+        consumed: IntArray?,
+        offsetInWindow: IntArray?
+    ): Boolean {
+        return mChildHelper.dispatchNestedPreScroll(dx, dy, consumed, offsetInWindow)
+    }
+
+    override fun startNestedScroll(axes: Int): Boolean {
+        return mChildHelper.startNestedScroll(axes)
+    }
+
+    override fun stopNestedScroll() {
+        mChildHelper.stopNestedScroll()
+    }
+
+    override fun hasNestedScrollingParent(): Boolean {
+        return mChildHelper.hasNestedScrollingParent()
+    }
+
+    override fun dispatchNestedFling(
+        velocityX: Float,
+        velocityY: Float,
+        consumed: Boolean
+    ): Boolean {
+        return mChildHelper.dispatchNestedFling(velocityX, velocityY, consumed)
+    }
+
+    override fun dispatchNestedPreFling(
+        velocityX: Float,
+        velocityY: Float
+    ): Boolean {
+        return mChildHelper.dispatchNestedPreFling(velocityX, velocityY)
+    }
+}
